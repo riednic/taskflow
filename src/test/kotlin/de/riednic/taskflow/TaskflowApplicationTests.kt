8944238ec3da4caa5b7e0189e401e9b481ec9c01
Nico <riedl.nico@yahoo.com>
2026-07-07 17:21:34 +0200

    bringup

---
+++ src/test/kotlin/de/riednic/taskflow/TaskflowApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.riednic.taskflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskflowApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
