112b7f71ef2adf8d109ed825a7b4280eadbf006d
Nico <riedl.nico@yahoo.com>
2026-07-09 21:41:39 +0200

    Added flyway migration

---
+++ src/main/resources/db/migration/V1__create_users.sql
@@ -0,0 +1,17 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+
+CREATE TYPE user_role AS ENUM (
+    'ADMIN',
+    'MEMBER',
+    'REVIEWER'
+);
+
+CREATE TABLE users
+(
+    id            BIGSERIAL PRIMARY KEY,
+    name          VARCHAR(100) NOT NULL,
+    email         CITEXT NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    role          user_role    NOT NULL DEFAULT 'MEMBER',
+    created_at    TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
