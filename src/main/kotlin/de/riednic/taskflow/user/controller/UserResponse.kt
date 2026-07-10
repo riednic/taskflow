7241b93478f60298d3a3047fc7b07b2c5c431a90
Nico <riedl.nico@yahoo.com>
2026-07-10 23:26:30 +0200

    Implemented controller for user endpoints

--- src/main/kotlin/de/riednic/taskflow/user/controller/UserResponse.kt
+++ src/main/kotlin/de/riednic/taskflow/user/controller/UserResponse.kt
@@ -1,10 +1,11 @@
 package de.riednic.taskflow.user.controller
 
 import de.riednic.taskflow.user.domain.UserRole
+import java.util.UUID
 import kotlin.time.Instant
 
 data class UserResponse(
-    val id: Long,
+    val id: UUID,
     val name: String,
     val email: String,
     val role: UserRole,
