cdaf852588be7982e1d2e5655e633068443895ec
Nico <riedl.nico@yahoo.com>
2026-07-10 18:26:00 +0200

    Basic detekt setup

--- src/main/kotlin/de/riednic/taskflow/user/application/UserService.kt
+++ src/main/kotlin/de/riednic/taskflow/user/application/UserService.kt
@@ -3,5 +3,4 @@ package de.riednic.taskflow.user.application
 import org.springframework.stereotype.Service
 
 @Service
-class UserService {
-}
+class UserService
--- src/test/kotlin/de/riednic/taskflow/TestDemoApplication.kt
+++ src/test/kotlin/de/riednic/taskflow/TestDemoApplication.kt
@@ -3,7 +3,6 @@ package de.riednic.taskflow
 import org.springframework.boot.fromApplication
 import org.springframework.boot.with
 
-
 fun main(args: Array<String>) {
-	fromApplication<TaskflowApplication>().with(TestcontainersConfiguration::class).run(*args)
+    fromApplication<TaskflowApplication>().with(TestcontainersConfiguration::class).run(*args)
 }
