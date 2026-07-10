5c6e88206b911374d2da30bc4f69aaf9f3c172f6
Nico <riedl.nico@yahoo.com>
2026-07-10 23:26:54 +0200

    Implemented orchestration layer for user endpoint

---
+++ src/main/kotlin/de/riednic/taskflow/user/application/UserRepository.kt
@@ -0,0 +1,13 @@
+package de.riednic.taskflow.user.application
+
+import de.riednic.taskflow.user.domain.User
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import java.util.UUID
+
+interface UserRepository {
+    fun findById(id: UUID): User?
+    fun findByEmail(email: String): User?
+    fun findAll(pageable: Pageable): Page<User>
+    fun save(user: User): User
+}
--- src/main/kotlin/de/riednic/taskflow/user/application/UserService.kt
+++ src/main/kotlin/de/riednic/taskflow/user/application/UserService.kt
@@ -1,6 +1,44 @@
 package de.riednic.taskflow.user.application
 
+import de.riednic.taskflow.user.controller.CreateUserRequest
+import de.riednic.taskflow.user.domain.EmailAlreadyExistsException
+import de.riednic.taskflow.user.domain.User
+import de.riednic.taskflow.user.domain.UserNotFoundException
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import java.time.Instant
+import java.util.UUID
+import kotlin.time.toKotlinInstant
 
 @Service
-class UserService
+class UserService(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder,
+) {
+
+    fun createUser(request: CreateUserRequest): User {
+        if (userRepository.findByEmail(request.email) != null) {
+            throw EmailAlreadyExistsException(request.email)
+        }
+
+        val newUser = User(
+            name = request.name,
+            email = request.email,
+            passwordHash = requireNotNull(passwordEncoder.encode(request.rawPassword)),
+            role = request.role,
+            createdAt = Instant.now().toKotlinInstant(),
+        )
+
+        return userRepository.save(newUser)
+    }
+
+    fun getUserById(id: UUID): User {
+        return userRepository.findById(id) ?: throw UserNotFoundException(id)
+    }
+
+    fun getUsers(pageable: Pageable): Page<User> {
+        return userRepository.findAll(pageable)
+    }
+}
