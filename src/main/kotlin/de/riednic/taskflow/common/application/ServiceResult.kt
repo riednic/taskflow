8275617881dc6b947d4f67aa6b540066f873ee6d
Nico <riedl.nico@yahoo.com>
2026-07-13 20:12:44 +0200

    Reworked ServiceResult structure

---
+++ src/main/kotlin/de/riednic/taskflow/common/application/ServiceResult.kt
@@ -0,0 +1,68 @@
+package de.riednic.taskflow.common.application
+
+import de.riednic.taskflow.common.persistence.RepositoryResult
+
+sealed interface ServiceResult<out T> {
+
+    sealed interface Success<out T> : ServiceResult<T> {
+        val value: T
+    }
+
+    data class Ok<out T>(override val value: T) : Success<T>
+    data object Deleted : Success<Unit> {
+        override val value = Unit
+    }
+
+    interface Error : ServiceResult<Nothing> {
+        val code: String
+        val category: ErrorCategory
+        val message: String
+        val throwable: Throwable? get() = null
+    }
+
+    data class NotFound(override val message: String = "Resource not found.") : Error {
+        override val code = "NOT_FOUND"
+        override val category = ErrorCategory.NOT_FOUND
+    }
+
+    data class ValidationError(override val message: String) : Error {
+        override val code = "VALIDATION_ERROR"
+        override val category = ErrorCategory.BAD_REQUEST
+    }
+
+    data class Forbidden(override val message: String) : Error {
+        override val code = "FORBIDDEN"
+        override val category = ErrorCategory.FORBIDDEN
+    }
+
+    data class Conflict(override val message: String, override val throwable: Throwable? = null) : Error {
+        override val code = "CONFLICT"
+        override val category = ErrorCategory.CONFLICT
+    }
+
+    data class VersionConflict(override val message: String, override val throwable: Throwable? = null) : Error {
+        override val code = "VERSION_CONFLICT"
+        override val category = ErrorCategory.CONFLICT
+    }
+
+    data class UnexpectedError(override val message: String, override val throwable: Throwable? = null) : Error {
+        override val code = "INTERNAL_ERROR"
+        override val category = ErrorCategory.INTERNAL
+    }
+}
+
+enum class ErrorCategory {
+    NOT_FOUND,
+    BAD_REQUEST,
+    FORBIDDEN,
+    CONFLICT,
+    INTERNAL,
+}
+
+fun <T> RepositoryResult<T>.toServiceResult(): ServiceResult<T> = when (this) {
+    is RepositoryResult.Success -> ServiceResult.Ok(value)
+    is RepositoryResult.NotFound -> ServiceResult.NotFound()
+    is RepositoryResult.Conflict -> ServiceResult.Conflict(message, throwable)
+    is RepositoryResult.VersionConflict -> ServiceResult.VersionConflict(message, throwable)
+    is RepositoryResult.UnexpectedError -> ServiceResult.UnexpectedError(message, throwable)
+}
