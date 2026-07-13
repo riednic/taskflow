1d7a20c4ed4408f9f2caee64adeb872afe416a2f
Nico <riedl.nico@yahoo.com>
2026-07-13 21:04:03 +0200

    Implemented task status transition in application layer

--- src/main/kotlin/de/riednic/taskflow/task/application/TaskRepository.kt
+++ src/main/kotlin/de/riednic/taskflow/task/application/TaskRepository.kt
@@ -1,10 +1,11 @@
 package de.riednic.taskflow.task.application
 
-import de.riednic.taskflow.common.RepositoryResult
+import de.riednic.taskflow.common.persistence.RepositoryResult
 import de.riednic.taskflow.task.domain.NewTask
 import de.riednic.taskflow.task.domain.ReplacementTask
 import de.riednic.taskflow.task.domain.Task
 import de.riednic.taskflow.task.domain.TaskFilter
+import de.riednic.taskflow.task.domain.TaskStatus
 import de.riednic.taskflow.task.domain.UpdatedTask
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -16,5 +17,6 @@ interface TaskRepository {
     fun save(newTask: NewTask): RepositoryResult<Task>
     fun replace(replacementTask: ReplacementTask): RepositoryResult<Task>
     fun update(updatedTask: UpdatedTask): RepositoryResult<Task>
-    fun delete(id: Long): RepositoryResult<Nothing>
+    fun transition(taskId: Long, targetStatus: TaskStatus, expectedVersion: Long): RepositoryResult<Task>
+    fun delete(id: Long): RepositoryResult<Unit>
 }
--- src/main/kotlin/de/riednic/taskflow/task/application/TaskService.kt
+++ src/main/kotlin/de/riednic/taskflow/task/application/TaskService.kt
@@ -1,7 +1,9 @@
 package de.riednic.taskflow.task.application
 
-import de.riednic.taskflow.common.ServiceResult
-import de.riednic.taskflow.common.toServiceResult
+import de.riednic.taskflow.common.persistence.RepositoryResult
+import de.riednic.taskflow.common.application.ServiceResult
+import de.riednic.taskflow.common.application.toServiceError
+import de.riednic.taskflow.common.application.toServiceResult
 import de.riednic.taskflow.task.controller.CreateTaskRequest
 import de.riednic.taskflow.task.controller.ReplaceTaskRequest
 import de.riednic.taskflow.task.controller.TransitionTaskStatusRequest
@@ -9,9 +11,14 @@ import de.riednic.taskflow.task.controller.UpdateTaskRequest
 import de.riednic.taskflow.task.domain.NewTask
 import de.riednic.taskflow.task.domain.ReplacementTask
 import de.riednic.taskflow.task.domain.Task
+import de.riednic.taskflow.task.domain.TaskEnteredReviewEvent
 import de.riednic.taskflow.task.domain.TaskFilter
+import de.riednic.taskflow.task.domain.TaskStatus
+import de.riednic.taskflow.task.domain.TaskStatusTransitionPolicy
 import de.riednic.taskflow.task.domain.UpdatedTask
+import de.riednic.taskflow.task.domain.isValidTransitionTo
 import de.riednic.taskflow.user.domain.UserRole
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
@@ -20,7 +27,9 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional
 class TaskService(
-    private val taskRepository: TaskRepository
+    private val taskRepository: TaskRepository,
+    private val taskAuditLogRepository: TaskAuditLogRepository,
+    private val eventPublisher: ApplicationEventPublisher,
 ) {
 
     @Transactional(readOnly = true)
@@ -83,7 +92,7 @@ class TaskService(
         return taskRepository.update(updatedTask).toServiceResult()
     }
 
-    fun deleteTask(taskId: Long): ServiceResult<Nothing> {
+    fun deleteTask(taskId: Long): ServiceResult<Unit> {
         return taskRepository.delete(taskId).toServiceResult()
     }
 
@@ -93,6 +102,37 @@ class TaskService(
         requestingUserId: Long,
         requestingRole: UserRole,
     ): ServiceResult<Task> {
-        TODO("Implement")
+        val savedTask = when (val current = taskRepository.findById(taskId)) {
+            is RepositoryResult.Error -> return current.toServiceError()
+            is RepositoryResult.Success -> current.value
+        }
+
+        if (!TaskStatusTransitionPolicy.isAllowed(requestingRole, requestingUserId, savedTask, request.status)) {
+            return ServiceResult.Forbidden(
+                "User $requestingUserId is not allowed to transition task $taskId to ${request.status}."
+            )
+        }
+        if (!savedTask.status.isValidTransitionTo(request.status)) {
+            return InvalidTransitionError(
+                "Transition from ${savedTask.status} to ${request.status} is not allowed."
+            )
+        }
+
+        val transitionResult = taskRepository.transition(taskId, request.status, request.version)
+        val transitionedTask = when (transitionResult) {
+            is RepositoryResult.Error -> return transitionResult.toServiceError()
+            is RepositoryResult.Success -> transitionResult.value
+        }
+
+        val auditResult = taskAuditLogRepository.record(taskId, savedTask.status, request.status, requestingUserId)
+        if (auditResult is RepositoryResult.Error) {
+            return auditResult.toServiceError()
+        }
+
+        if (request.status == TaskStatus.IN_REVIEW) {
+            eventPublisher.publishEvent(TaskEnteredReviewEvent(taskId))
+        }
+
+        return ServiceResult.Ok(transitionedTask)
     }
 }
